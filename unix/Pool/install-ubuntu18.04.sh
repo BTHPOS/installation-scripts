aba4e7a7b75d8cbed3999d868f469d0b8f81434c
Dondrey Taylor <dondrey.taylor@gmail.com>
2018-10-01 16:50:06 -0400

    Added pool installation script

---
+++ unix/Pool/install-ubuntu18.04.sh
@@ -0,0 +1,10 @@
+sudo apt-get update -y
+sudo apt-get install npm -y
+sudo npm install n -g -y
+sudo n v7
+sudo apt update
+sudo apt install redis-server -y
+git clone https://github.com/BTHPOS/pool-z-nomp.git pool
+cd pool
+npm update
+npm install
