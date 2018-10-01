dbeefffc13aad564a5b88c9e83cfae46f118ab2c
Dondrey Taylor <dondrey.taylor@gmail.com>
2018-09-30 23:09:25 -0400

    Added script for DNS

---
+++ unix/DNS/install.sh
@@ -0,0 +1,7 @@
+sudo apt-get update -y
+sudo apt-get install npm -y
+sudo npm install n -g -y
+sudo n v9
+git clone https://github.com/BTHPOS/dns-seed-handler.git dns-seed-handler
+cd dns-seed-handler
+npm install
