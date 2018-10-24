9c3a8360e8b7c2e058b3dfddf2423d6f14f935c5
Dondrey Taylor <dondrey.taylor@gmail.com>
2018-10-23 21:35:18 -0400

    Removed custom.sh since EthOS 1.3.3 comes w/ lolMiner

--- ethos/lolMiner-AMD/install.sh
+++ ethos/lolMiner-AMD/install.sh
@@ -2,6 +2,11 @@
 ## Installing lolMiner on EthOS- EthOS ##
 #########################################
 
+#
+# MUST HAVE EthOS 1.3.3 or higher  
+#
+$build=lolMiner_v05_Lin64.tar.gz
+
 # Stop any current mining
 minestop
 
@@ -10,21 +15,16 @@ git clone https://github.com/BTHPOS/mining-amd-lolminer.git
 
 # Remove the files that we don't need
 mkdir lolMiner
-tar -xvzf mining-amd-lolminer/builds/lolMiner_v05_Lin64.tar.gz -C lolMiner
+tar -xvzf mining-amd-lolminer/builds/$build -C lolMiner
 rm -rf lolMiner/user_config.json
 rm -rf lolMiner/run_miner_with_restart.sh
 rm -rf lolMiner/run_miner.sh.sh
 rm -rf lolMiner/run_benchmark.sh.sh
 rm -rf mining-amd-lolminer
-rm -rf /opt/miners/lolMiner > /dev/null
+
+if [ ! -d "/opt/miners/lolMiner" ]; then
+  mv /opt/miners/lolMiner /opt/miners/lolMiner.BAK
+fi
 
 # Move the lolMiner build to where the rest of the miners are
 sudo mv lolMiner /opt/miners/
-
-# Backup the current custom configuration
-mv ~/custom.sh ~/custom.sh.disabled > /dev/null
-
-# Move the new config to the home directory
-cp ./lolMiner.conf ~/lolMiner.conf
-cp ./custom.sh ~/custom.sh
-chmod +x ~/custom.sh
