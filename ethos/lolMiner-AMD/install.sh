e25764c1bc5e40e20beb0e10b0615476044cb8ed
Dondrey Taylor <dondrey.taylor@gmail.com>
2018-10-23 21:57:40 -0400

    Fixing script errors

--- ethos/lolMiner-AMD/install.sh
+++ ethos/lolMiner-AMD/install.sh
@@ -16,17 +16,17 @@ minestop
 git clone https://github.com/BTHPOS/mining-amd-lolminer.git
 
 # Remove the files that we don't need
-mkdir lolMiner
-tar -xvzf mining-amd-lolminer/builds/$build -C lolMiner
-rm -rf lolMiner/user_config.json
-rm -rf lolMiner/run_miner_with_restart.sh
-rm -rf lolMiner/run_miner.sh.sh
-rm -rf lolMiner/run_benchmark.sh.sh
+mkdir lolminer
+tar -xvzf mining-amd-lolminer/builds/$build -C lolminer
+rm -rf lolminer/user_config.json
+rm -rf lolminer/run_miner_with_restart.sh
+rm -rf lolminer/run_miner.sh.sh
+rm -rf lolminer/run_benchmark.sh.sh
 rm -rf mining-amd-lolminer
 
-if [ ! -d "/opt/miners/lolMiner" ]; then
-  echo "lolMiner already exists. Backing up and replacing"
-  mv /opt/miners/lolMiner /opt/miners/lolMiner.BAK
+if [ ! -d "/opt/miners/lolminer" ]; then
+  echo "lolminer already exists. Backing up and replacing"
+  sudo mv /opt/miners/lolminer /opt/miners/lolminer.BAK
 fi
 
 # Move the lolMiner build to where the rest of the miners are
