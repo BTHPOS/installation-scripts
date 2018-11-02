41e20e148dc05ead504b2df58a131e5a7fceefa2
Dondrey Taylor <dondrey.taylor@gmail.com>
2018-11-02 00:32:25 -0400

    Added dependencies to install script

--- unix/Pool/install-ubuntu18.04.sh
+++ unix/Pool/install-ubuntu18.04.sh
@@ -1,9 +1,49 @@
+# Update system
 sudo apt-get update -y
+
+# boostlib
+sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev -y
+
+# open ssl
+sudo apt-get install libssl-dev -y
+
+# libevent
+sudo apt-get install libevent-dev -y
+
+# cURL and LibTools
+sudo apt-get install curl git build-essential libtool autotools-dev -y
+
+# Setup python3 other utils
+sudo apt-get install automake pkg-config bsdmainutils python3 -y
+
+# Libx11
+sudo apt-get install libx11-xcb-dev libfontconfig-dev -y
+
+# Install Libsodium
+wget https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
+tar -xf LATEST.tar.gz
+rm -rf LATEST.tar.gz
+cd libsodium-stable/
+./configure
+make && make check
+sudo make install
+cd ..
+rm -rf libsodium-stable
+sudo ln -s /usr/local/lib/libsodium.so.23 /usr/lib/libsodium.so.23
+
+# Install NPM and node
 sudo apt-get install npm -y
 sudo npm install n -g -y
 sudo n v7
+
+
+# Update system
 sudo apt update
+
+# Install Redis
 sudo apt install redis-server -y
+
+# Get pool code and set it up
 git clone https://github.com/BTHPOS/pool-z-nomp.git pool
 cd pool
 npm update
