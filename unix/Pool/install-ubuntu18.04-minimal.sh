f15f824e0b17cbef960c90eaa35b15fcde878f15
Dondrey Taylor <dondrey.taylor@gmail.com>
2019-01-03 16:23:18 -0500

    Update and rename install-ubuntu18.04.sh to install-ubuntu18.04-minimal.sh

--- unix/Pool/install-ubuntu18.04-minimal.sh
+++ unix/Pool/install-ubuntu18.04-minimal.sh
@@ -1,6 +1,18 @@
 # Update system
 sudo apt-get update
 
+# Install Libsodium
+wget https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
+tar -xf LATEST.tar.gz
+rm -rf LATEST.tar.gz
+cd libsodium-stable/
+./configure
+sudo make && make check
+sudo make install
+cd ..
+sudo rm -rf libsodium-stable
+sudo ln -s /usr/local/lib/libsodium.so.23 /usr/lib/libsodium.so.13
+
 # Install NPM and node
 sudo apt-get install npm -y
 sudo npm install n -g -y
