76c2fc84e8070e707316171516884ad62cf27d4c
Dondrey Taylor <dondrey.taylor@gmail.com>
2019-01-04 17:32:19 -0500

    Included BerkelyDB 4.8

--- unix/BTH/install-windows-32bit-crosscompile.sh
+++ unix/BTH/install-windows-32bit-crosscompile.sh
@@ -7,6 +7,11 @@ sudo apt install nsis -y
 # Set the default mingw32 g++ compiler option to posix.
 sudo update-alternatives --config i686-w64-mingw32-g++  -y
 
+# Install BerkelyDB
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:bitcoin/bitcoin
+sudo apt-get update -y
+sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
 
 # Clone BTH
 cd /$1
--- unix/BTH/install-windows-64bit-crosscompile.sh
+++ unix/BTH/install-windows-64bit-crosscompile.sh
@@ -7,6 +7,12 @@ sudo apt install nsis -y
 # Set the default mingw32 g++ compiler option to posix.
 sudo update-alternatives --config x86_64-w64-mingw32-g++ -y
 
+# Install BerkelyDB
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:bitcoin/bitcoin
+sudo apt-get update -y
+sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
+
 # Clone BTH
 cd /$1
 git clone https://github.com/BTHPOS/BTH.git BTH
