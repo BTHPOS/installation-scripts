b448e97868a9fe7e3132c1acaac418a642978e77
Dondrey Taylor <dondrey.taylor@gmail.com>
2019-01-06 14:16:52 -0500

    Added libsodium to BTH windows 32

--- unix/BTH/install-windows-32bit-crosscompile.sh
+++ unix/BTH/install-windows-32bit-crosscompile.sh
@@ -27,6 +27,18 @@ sudo apt-get install automake pkg-config bsdmainutils python3 -y
 # Libx11
 sudo apt-get install libx11-xcb-dev libfontconfig-dev -y
 
+# Install Libsodium
+wget https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
+tar -xf LATEST.tar.gz
+rm -rf LATEST.tar.gz
+cd libsodium-stable/
+./configure
+make && make check
+sudo make install
+cd ..
+rm -rf libsodium-stable
+sudo ln -s /usr/local/lib/libsodium.so.23 /usr/lib/libsodium.so.23
+
 # Install BerkelyDB
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:bitcoin/bitcoin
