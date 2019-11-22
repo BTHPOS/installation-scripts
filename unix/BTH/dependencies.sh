99b295508ee52162fc4ce76f7a486e519b9d97fb
Dondrey Taylor <dondrey.taylor@gmail.com>
2019-11-22 01:02:09 -0500

    Update dependencies.sh

--- unix/BTH/dependencies.sh
+++ unix/BTH/dependencies.sh
@@ -1,3 +1,41 @@
+# Update system
+sudo apt-get update -y
+
+# Install git
+sudo apt-get install git -y
+
+# boostlib
+sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev -y
+
+# open ssl
+sudo apt-get install libssl-dev -y
+
+# libevent
+sudo apt-get install libevent-dev -y
+
+# cURL and LibTools
+sudo apt-get install curl git build-essential libtool autotools-dev -y
+
+# Setup python3 other utils
+sudo apt-get install automake pkg-config bsdmainutils python3 -y
+
+# Libx11
+sudo apt-get install libx11-xcb-dev libfontconfig-dev -y
+
+# Install Libsodium
+wget https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
+tar -xf LATEST.tar.gz
+rm -rf LATEST.tar.gz
+cd libsodium-stable/
+./configure
+make && make check
+sudo make install
+cd ..
+rm -rf libsodium-stable
+sudo ln -s /usr/local/lib/libsodium.so.23 /usr/lib/libsodium.so.23
+
+# Install unzip
+sudo apt-get install unzip -y
 
 
 sudo add-apt-repository ppa:bitcoin/bitcoin
